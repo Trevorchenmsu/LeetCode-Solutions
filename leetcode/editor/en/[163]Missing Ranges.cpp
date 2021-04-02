de8631a8f696f9b6852fa4b54b948ae40e85e1b0
jchen929 <jishengchen3120@gatech.edu>
2021-04-02 10:02:55 -0400

    upload 219,350,448,628,724,977,1710

---
+++ leetcode/editor/en/[163]Missing Ranges.cpp
@@ -0,0 +1,103 @@
+//You are given an inclusive range [lower, upper] and a sorted unique integer ar
+//ray nums, where all elements are in the inclusive range. 
+//
+// A number x is considered missing if x is in the range [lower, upper] and x is
+// not in nums. 
+//
+// Return the smallest sorted list of ranges that cover every missing number exa
+//ctly. That is, no element of nums is in any of the ranges, and each missing numb
+//er is in one of the ranges. 
+//
+// Each range [a,b] in the list should be output as: 
+//
+// 
+// "a->b" if a != b 
+// "a" if a == b 
+// 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [0,1,3,50,75], lower = 0, upper = 99
+//Output: ["2","4->49","51->74","76->99"]
+//Explanation: The ranges are:
+//[2,2] --> "2"
+//[4,49] --> "4->49"
+//[51,74] --> "51->74"
+//[76,99] --> "76->99"
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [], lower = 1, upper = 1
+//Output: ["1"]
+//Explanation: The only missing range is [1,1], which becomes "1".
+// 
+//
+// Example 3: 
+//
+// 
+//Input: nums = [], lower = -3, upper = -1
+//Output: ["-3->-1"]
+//Explanation: The only missing range is [-3,-1], which becomes "-3->-1".
+// 
+//
+// Example 4: 
+//
+// 
+//Input: nums = [-1], lower = -1, upper = -1
+//Output: []
+//Explanation: There are no missing ranges since there are no missing numbers.
+// 
+//
+// Example 5: 
+//
+// 
+//Input: nums = [-1], lower = -2, upper = -1
+//Output: ["-2"]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// -109 <= lower <= upper <= 109 
+// 0 <= nums.length <= 100 
+// lower <= nums[i] <= upper 
+// All the values of nums are unique. 
+// 
+// Related Topics Array 
+// 👍 518 👎 2106
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+ * solution: array
+ * time: O(n)
+ * space: O(1)
+ *
+ * */
+class Solution {
+public:
+    vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
+        vector<string> res;
+
+        for (int num : nums) {
+            if (num > lower)
+                res.push_back(to_string(lower) + (num - 1 > lower ? ("->" + to_string(num - 1)) : ""));
+
+            if (num == upper)
+                return res;
+
+            lower = num + 1;
+        }
+
+        if (lower <= upper)
+            res.push_back(to_string(lower) + (upper > lower ? ("->" + to_string(upper)) : ""));
+
+        return res;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/en/[1793]Maximum Score of a Good Subarray.cpp
@@ -0,0 +1,58 @@
+//You are given an array of integers nums (0-indexed) and an integer k. 
+//
+// The score of a subarray (i, j) is defined as min(nums[i], nums[i+1], ..., num
+//s[j]) * (j - i + 1). A good subarray is a subarray where i <= k <= j. 
+//
+// Return the maximum possible score of a good subarray. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [1,4,3,7,4,5], k = 3
+//Output: 15
+//Explanation: The optimal subarray is (1, 5) with a score of min(4,3,7,4,5) * (
+//5-1+1) = 3 * 5 = 15. 
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [5,5,4,5,4,1,1,1], k = 0
+//Output: 20
+//Explanation: The optimal subarray is (0, 4) with a score of min(5,5,4,5,4) * (
+//4-0+1) = 4 * 5 = 20.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 105 
+// 1 <= nums[i] <= 2 * 104 
+// 0 <= k < nums.length 
+// 
+// Related Topics Greedy 
+// 👍 225 👎 14
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int maximumScore(vector<int>& nums, int k) {
+        int res = nums[k], min_val = nums[k],  n = nums.size();
+        int i = k, j = k;
+
+        while (i > 0 or j < n - 1) {
+            if ((i > 0 ? nums[i - 1] : 0) < (j < n - 1 ? nums[j + 1] : 0))
+                min_val = min(min_val, nums[++j]);
+            else
+                min_val = min(min_val, nums[--i]);
+
+            res = max(res, min_val * (j - i + 1));
+        }
+
+        return res;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
