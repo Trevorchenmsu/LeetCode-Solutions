72fbc6734d0fe82ee60114251ebe6c148ebcc51f
jchen929 <jishengchen3120@gatech.edu>
2021-05-12 10:34:32 -0400

    upload 327,424

---
+++ leetcode/editor/en/[327]Count of Range Sum.cpp
@@ -0,0 +1,96 @@
+//Given an integer array nums and two integers lower and upper, return the numbe
+//r of range sums that lie in [lower, upper] inclusive. 
+//
+// Range sum S(i, j) is defined as the sum of the elements in nums between indic
+//es i and j inclusive, where i <= j. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums = [-2,5,-1], lower = -2, upper = 2
+//Output: 3
+//Explanation: The three ranges are: [0,0], [2,2], and [0,2] and their respectiv
+//e sums are: -2, -1, 2.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums = [0], lower = 0, upper = 0
+//Output: 1
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 104 
+// -231 <= nums[i] <= 231 - 1 
+// -105 <= lower <= upper <= 105 
+// The answer is guaranteed to fit in a 32-bit integer. 
+// 
+// Related Topics Binary Search Divide and Conquer Sort Binary Indexed Tree Segm
+//ent Tree 
+// 👍 1054 👎 122
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+ * solution 1: divide and conquer
+ * time: O(nlog(logn))
+ * space: O(n)
+ * */
+class Solution {
+public:
+    int countRangeSum(vector<int>& nums, int lower, int upper) {
+        int n = nums.size(), res = 0;
+        vector<long> presum(n + 1);
+        for (int i = 0; i < n; i++)
+            presum[i + 1] = presum[i] + nums[i];
+
+        divideConquer(presum, 0, n, lower, upper, res);
+
+        return res;
+    }
+
+private:
+    vector<long> temp = vector<long> (100001);
+    void divideConquer(vector<long> &presum, int start, int end, int lower, int upper, int &res) {
+        if (start == end)
+            return;
+
+        int mid = start + (end -  start) / 2;
+        divideConquer(presum, start, mid, lower, upper, res);
+        divideConquer(presum, mid + 1, end, lower, upper, res);
+
+        for (int i = start; i <= mid; i++) {
+            auto p1 = lower_bound(presum.begin() + mid + 1, presum.begin() + end + 1, presum[i] + lower);
+            auto p2 = upper_bound(presum.begin() + mid + 1, presum.begin() + end + 1, presum[i] + upper);
+            res += p2 - p1;
+        }
+
+        mergeSort(start, mid, end, presum);
+    }
+
+    void mergeSort(int start, int mid, int end, vector<long> &presum) {
+        int i = start, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= end) {
+            if (presum[i] <= presum[j])
+                temp[k++] = presum[i++];
+            else
+                temp[k++] = presum[j++];
+        }
+
+        while (i <= mid)
+            temp[k++] = presum[i++];
+
+        while (j <= end)
+            temp[k++] = presum[j++];
+
+        for (int i = 0; i < end - start + 1; i++)
+            presum[start + i] = temp[i];
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/en/[424]Longest Repeating Character Replacement.cpp
@@ -0,0 +1,95 @@
+//You are given a string s and an integer k. You can choose any character of the
+// string and change it to any other uppercase English character. You can perform 
+//this operation at most k times. 
+//
+// Return the length of the longest substring containing the same letter you can
+// get after performing the above operations. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "ABAB", k = 2
+//Output: 4
+//Explanation: Replace the two 'A's with two 'B's or vice versa.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "AABABBA", k = 1
+//Output: 4
+//Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+//The substring "BBBB" has the longest repeating letters, which is 4.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length <= 105 
+// s consists of only uppercase English letters. 
+// 0 <= k <= s.length 
+// 
+// Related Topics Two Pointers Sliding Window 
+// 👍 2406 👎 120
+
+
+//Input: s = "AABABBA", k = 1
+//Output: 4
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+ * solution 1: two pointers / sliding window
+ * time: O(n)
+ * space:O(1)
+ * */
+//class Solution {
+//public:
+//    int characterReplacement(string s, int k) {
+//        int left = 0, res = 0;
+//        vector<int> count(26, 0);
+//
+//        for (int right = 0; right < s.size(); right++) {
+//            count[s[right] - 'A']++;
+//
+//            while (right - left + 1 - *max_element(count.begin(), count.end()) > k) {
+//                count[s[left] - 'A']--;
+//                left++;
+//            }
+//
+//            res = max(res, right - left + 1);
+//        }
+//
+//        return res;
+//    }
+//};
+
+/*
+ * solution 2: two pointers / sliding window
+ * time: O(n)
+ * space:O(1)
+ * */
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int left = 0, res = 0;
+        vector<int> count(26, 0);
+
+        for (int right = 0; right < s.size(); right++) {
+            count[s[right] - 'A']++;
+            int maxFreq = *max_element(count.begin(), count.end());
+
+            if (right - left + 1 - maxFreq <= k) {
+                res = max(res, right - left + 1);
+            }
+            else {
+                count[s[left] - 'A']--;
+                left++;
+            }
+        }
+
+        return res;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
